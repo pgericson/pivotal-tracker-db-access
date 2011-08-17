3bd4d354b186a41cb11b94ca7768ce8965276031
Peter Ericson <pg.ericson@gmail.com>
2011-08-17 10:44:07 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PivotalTrackerDbAccess::Application.config.secret_token = '4b258659209aa338f4fb1b995a1ff45959d1ccdc497d3f7fb3c108c361a4fe74f22794028d365082a8ed70568c57851b256ad46d4a453e60929515460ec0e17b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PivotalTrackerDbAccess::Application.config.session_store :cookie_store, :key => '_pivotal_tracker_db_access_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PivotalTrackerDbAccess::Application.config.session_store :active_record_store
